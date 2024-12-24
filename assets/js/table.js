a112e55a6d3b3d8ad47fd01eae117b69ef0afbd7
HarborZeng <harbourzeng@gmail.com>
2024-12-24 23:47:08 +0800

    vault backup: 2024-12-24 23:47:08

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,6 +1,10 @@
 // 获取表格元素
 const tables = document.getElementsByTagName('table');
 for (const table of tables) {
+  const d = document.createElement('div');
+  d.classList.add('table-container')
+  table.parentNode.insertBefore(d, table);
+  d.appendChild(table);
   // 使用querySelectorAll结合CSS选择器获取第一列的td元素
   const firstColumnTds = table.querySelectorAll('tr td:first-child');
   // 可以遍历获取到的元素集合，添加class
